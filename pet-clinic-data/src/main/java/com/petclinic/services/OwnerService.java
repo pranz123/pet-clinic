717860f5beda58ced2f1dff49054aa2e24fc5d8d
pranjal <pranjalkumar@ymail.com>
2018-09-23 16:24:39 +0530

    Implement Service Interface Closes #5

---
+++ pet-clinic-data/src/main/java/com/petclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package com.petclinic.services;
+
+import com.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
