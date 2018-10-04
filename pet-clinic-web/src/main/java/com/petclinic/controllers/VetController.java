2a994d821a84c5fd3524eb9663319b8d57c02401
pranjal <pranjalkumar@ymail.com>
2018-10-04 20:18:24 +0530

    Show vet and owner data
    Closes #16
    Closes #17

--- pet-clinic-web/src/main/java/com/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.petclinic.controllers;
 
+import com.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/vetIndex","/vets/vetIndex.html"})
-    public String listsVets(){
+    public String listsVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/vetIndex";
     }
 }
