c374354c11ebb7a4a06ccb42991fa340bdd522db
pranjal <pranjalkumar@ymail.com>
2018-10-06 11:47:15 +0530

    Added PetMapService, PetTypeMapService, SpecialityMapService
    Closes #27
    Closes #28
    Closes #28

---
+++ pet-clinic-data/src/main/java/com/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.petclinic.services.map;
+
+import com.petclinic.model.Speciality;
+import com.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
