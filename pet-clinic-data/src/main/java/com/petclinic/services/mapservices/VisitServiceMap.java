a359c624568770d7e273c8ae834ab966f493950e
pranjal <pranjalkumar@ymail.com>
2018-10-20 16:29:28 +0530

    Implemented Spring JPA services For Visits

---
+++ pet-clinic-data/src/main/java/com/petclinic/services/mapservices/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.petclinic.services.mapservices;
+
+import com.petclinic.model.Visit;
+import com.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService{
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null ){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
