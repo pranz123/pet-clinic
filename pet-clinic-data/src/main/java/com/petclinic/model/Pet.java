e98d64cc8a8a4c622151bcd0a3fbe1206a98a8ef
pranjal <pranjalkumar@ymail.com>
2018-10-20 14:44:36 +0530

    Implemented owner entity
    Closes #34

--- pet-clinic-data/src/main/java/com/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Pet.java
@@ -1,15 +1,24 @@
 package com.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "Pets")
 public class Pet extends BaseEntity{
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @Column(name = "name")
     private String name;
 
     public PetType getPetType() {
